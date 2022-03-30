9d79e55d5a8c3fe7350d1db7b6df19e89a348b86
JenyaKh <93423968+JenyaKh@users.noreply.github.com>
2022-03-30 15:54:14 +0300

    added functionality hw3

--- HW3/script.js
+++ HW3/script.js
@@ -1,1 +1,42 @@
-let 
+let operator;
+let firstNumber; 
+let secondNumber; 
+
+function checkOperator (operator) {
+    const operators = ['+', '-', '*', '/']
+    let isOperator = operators.includes(operator) ? true: false;
+    return isOperator;
+};
+
+function calcResult (operator, firstNumber, secondNumber) {
+    let result;
+    switch (operator) {
+        case '+':
+           result = firstNumber + secondNumber;
+           break;
+        case '-':
+           result = firstNumber - secondNumber;
+           break;
+        case '*':
+           result = firstNumber * secondNumber;   
+           break; 
+        case '/':
+           result = firstNumber / secondNumber;
+    };
+    return result;
+}
+
+do {
+    operator = prompt('Choose what will we do? (+-*/');
+} while (checkOperator(operator) == false);
+
+do {
+   firstNumber = +prompt('Enter the first number');
+} while (isNaN(firstNumber));
+
+do {
+    secondNumber = +prompt('Enter the second number');
+ } while (isNaN(secondNumber));
+
+ alert(`result: ${firstNumber} ${operator} ${secondNumber} = ${calcResult(operator, firstNumber, secondNumber)}`)
+ 
